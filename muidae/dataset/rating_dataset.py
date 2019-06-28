fix normalization + log it

muidae/dataset/rating_dataset.py
@@ -175,28 +175,46 @@ class RatingDataset(PytorchDataset):
         if self._view == "item_view":
             
             if idx < 0 or idx > self.nb_item:
+
                 return 0
 
-            else:
+            elif self._is_normalized:
+
                 swap_idx = self.item_index_swap[idx]
+
                 data = self.data[:, swap_idx].todense()
                 mask = data != 0.0
                 bias = self.gm + self.um + self.im[swap_idx]
 
                 return np.ravel( data - np.multiply(bias,mask) )
 
+            else:
+
+                swap_idx = self.item_index_swap[idx]
+
+                return np.ravel(self.data[:, swap_idx].todense())
+
         elif self._view == "user_view":
             
             if idx < 0 or idx > self.nb_user:
+
                 return 0
 
-            else:
+            elif self._is_normalized:
+
                 swap_idx = self.user_index_swap[idx]
+
                 data = self.data[swap_idx, :].todense()
                 mask = data != 0.0
                 bias = self.gm + self.um[swap_idx] + self.im
 
                 return np.ravel( data - np.multiply(bias,mask) )
+
+            else:
+
+                swap_idx = self.user_index_swap[idx]
+
+                return np.ravel( self.data[swap_idx, :].todense() )
 
         else:
             return 0
@@ -226,10 +244,12 @@ class RatingDataset(PytorchDataset):
             self.um = np.divide( um_sum.astype(float), um_nnz, out=np.zeros_like(um_sum), where=um_nnz!=0 ) - self.gm
         
         if item_mean == True:
-            
+
             im_sum = self.data.sum(axis=0)
             im_nnz = self.data.getnnz(axis=0)
             self.im = np.ravel( np.divide( im_sum.astype(float), im_nnz, out=np.zeros_like(im_sum), where=im_nnz!=0 ) ) - self.gm
+
+        self._is_normalize = True
 
 
     """

muidae/main.py
@@ -116,9 +116,9 @@ if __name__ == "__main__":
 
             validation_batch = [validation_dataset[i*args.batch_size+j] for j in range(remaining)]
 
-            input_data = Variable( torch.Tensor( np.squeeze( np.stack( validation_batch ) ) ) )
+            input_data = Variable( torch.Tensor( np.squeeze( np.stack( validation_batch ) ) ) ).to(device)
 
-            output_data = my_base_dae( input_data.to(device) )
+            output_data = my_base_dae( input_data )
 
             mmse_loss = my_base_dae.get_mmse_loss(input_data, output_data)
 

muidae/model/base_dae.py
@@ -143,7 +143,7 @@ class BaseDAE(nn.Module):
 
         mask = input_data != 0.0
         nb_rating = torch.sum( mask )
-        loss = mmse_criterion( input_data, output_data * mask.float() ) / nb_rating
+        loss = mmse_criterion( input_data, output_data * mask.float() ) / nb_rating.float()
 
         return loss
 